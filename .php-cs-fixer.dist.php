10716e6bb8e0462d3a57ca486722e889fbea06a1
palPalani <palaniappan@innoppl.com>
2021-09-09 12:20:18 +0530

    Fixed CS

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,10 +3,10 @@
 $finder = Symfony\Component\Finder\Finder::create()
     ->in([
         __DIR__ . '/src',
-        __DIR__ . '/tests',
+        //__DIR__ . '/tests',
     ])
     ->name('*.php')
-    ->notName('*.blade.php')
+    //->notName('*.blade.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
